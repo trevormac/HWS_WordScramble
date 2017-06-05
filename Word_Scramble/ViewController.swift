2baf244831cd8dd62b93a65e37c31348d54c76ef
trevormac <trevor.c.macgregor@gmail.com>
2017-06-05 17:07:29 -0400

    Initial Commit

---
+++ Word_Scramble/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Word_Scramble
+//
+//  Created by Trevor MacGregor on 2017-06-05.
+//  Copyright © 2017 TeevoCo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
